527f9e301670400fad32957b10b9cba38c9d27be
unknown <you@example.com>
2021-01-17 12:15:53 +0800

    Update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,7 @@ const Footer = () => (
                 <div className="row">
                     <div className="col-12 pt-4 pb-3 text-center">
                         <p className="font-weight-bold mb-0">The End</p>
-                        <p className="with">Built with <a href="https://www.gatsbyjs.org/" target="_blank">Gatsby</a> and <a href="https://reactjs.org/" target="_blank">React</a>. Deployed on <a href="https://www.netlify.com/" target="_blank">Netlify</a>.</p>
+                        <p className="with">Built with <a href="https://www.gatsbyjs.org/" target="_blank">Gatsby</a>. Deployed on <a href="https://www.netlify.com/" target="_blank">Netlify</a>.</p>
                     </div>
                 </div>
             </div>
